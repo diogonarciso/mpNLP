12479936effeff02c94ee65d80abd8a4ef82475c
Diogo Narciso <45459868+diogonarciso@users.noreply.github.com>
2024-04-09 10:45:49 +0100

    Add files via upload
    Collection of mp-NLP examples. These are defined using Python files, where all relevant vectors and matrices necessary for the definition of mp-NLPs are coded.

---
+++ mp-NLPs/x2_z4_2.py
@@ -0,0 +1,28 @@
+# %% Libraries
+import numpy as np
+
+#%% Partial derivatives of objective function
+def partial_deriv(x):
+    return [2*x[0]**3 + 1/2*x[0]**2 + 4*x[0] - 13/2,
+            2*x[1]**3 + 1/2*x[1]**2 + 4*x[1] - 13/2]
+
+#%% Jacobian of partial derivatives function
+def jac_partial_deriv(x):
+    return np.array([[6*x[0]**2 + x[0] + 4, 0],
+                     [0, 6*x[1]**2 + x[1] + 4]])
+
+#%% Constraints on optimization variables (A x <= b + F theta):
+A = np.array([[1, 2], [1, 2], [2, 3], [3, 4]])
+b = np.array([2, 1, 4, 6])
+F = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+feas_space_constr = {'A':A, 'b':b, 'F':F}
+
+#%% Space of parameters P_A theta <= P_b:
+P_A = np.array([[-1, 0, 0, 0], [1, 0, 0, 0], [0, -1, 0, 0], [0, 1, 0, 0], [0, 0, -1, 0], [0, 0, 1, 0], [0, 0, 0, -1], [0, 0, 0, 1]])
+P_b = np.array([0, 2, 0, 3, 0, 2, 0, 2])
+
+par_space_constr = {'P_A':P_A, 'P_b':P_b}
+
+#%% Bounds for optimization problems
+bnds_theta = (0, 3)
---
+++ mp-NLPs/x3_z3_1.py
@@ -0,0 +1,30 @@
+# %% Libraries
+import numpy as np
+
+#%% Partial derivatives of objective function
+def partial_deriv(x):
+    return [2*x[0]**3 + 1/2*x[0]**2 + 4*x[0] - 13/2,
+            2*x[1]**3 + 1/2*x[1]**2 + 4*x[1] - 13/2,
+            2*x[2]**3 + 1/2*x[2]**2 + 4*x[2] - 13/2]
+
+#%% Jacobian of partial derivatives function
+def jac_partial_deriv(x):
+    return np.array([[6*x[0]**2 + x[0] + 4, 0, 0],
+                     [0, 6*x[1]**2 + x[1] + 4, 0],
+                     [0, 0, 6*x[2]**2 + x[2] + 4]])
+
+#%% Constraints on optimization variables (A x <= b + F theta):
+A = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+b = np.array([5, 8, 11])
+F = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+feas_space_constr = {'A':A, 'b':b, 'F':F}
+
+#%% Space of parameters P_A theta <= P_b:
+P_A = np.array([[-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]])
+P_b = np.array([0, 2, 0, 2, 0, 2])
+
+par_space_constr = {'P_A':P_A, 'P_b':P_b}
+
+#%% Bounds for optimization problems
+bnds_theta = (0, 2)
